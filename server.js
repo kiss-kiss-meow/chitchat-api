a3c41d93b3f960853ebbe5d486b20672004b1d67
Igor Moiseyenko <imoiseyenko93@gmail.com>
2017-10-04 18:06:12 +0300

    Using of WebSocket layer.

--- server.js
+++ server.js
@@ -2,13 +2,13 @@ const Hapi = require('hapi')
 const WebSocket = require('ws')
 const config = require('config')
 const api = require('./api')
+const ws = require('./ws')
 
 const { port } = config.get('server')
 const { port: wssPort } = config.get('wss')
+
 const server = new Hapi.Server()
-
 server.connection({ port })
-
 api.init(server)
 
 server.start(err => {
@@ -17,19 +17,4 @@ server.start(err => {
 })
 
 const wss = new WebSocket.Server({ port: wssPort })
-
-wss.on('connection', ws => {
-  console.log('Connection opened.')
-
-  ws.on('message', msg => {
-    // console.log(`received: ${msg}`)
-    wss.clients.forEach(client => {
-      if (client.readyState !== WebSocket.OPEN) return
-      client.send(msg)
-    })
-  })
-
-  ws.on('close', () => {
-    console.log('Connection closed.')
-  })
-})
+ws.init(wss)
