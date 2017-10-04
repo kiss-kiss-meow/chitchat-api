09f1f2e4ecf86105921756d8c721adaebd37a980
Igor Moiseyenko <imoiseyenko93@gmail.com>
2017-10-04 23:21:59 +0300

    Setting up AOP before server starts.

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 require('dotenv').config()
+
 const Hapi = require('hapi')
 const WebSocket = require('ws')
 const config = require('config')
 const api = require('./api')
 const ws = require('./ws')
+const aop = require('./aop')
 
 const { port } = config.get('server')
 const { port: wssPort } = config.get('wss')
@@ -12,10 +14,16 @@ const server = new Hapi.Server()
 server.connection({ port })
 api.init(server)
 
-server.start(err => {
-  if (err) throw err
-  console.log(`Server running at: ${server.info.uri}`)
-})
-
 const wss = new WebSocket.Server({ port: wssPort })
 ws.init(wss)
+
+Promise.all([aop.init({ server })])
+  .then(() => {
+    server.start(err => {
+      if (err) throw err
+      console.log(`Server running at: ${server.info.uri}`)
+    })
+  })
+  .catch(err => {
+    console.log(err)
+  })
