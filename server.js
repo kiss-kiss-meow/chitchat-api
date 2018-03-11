1fae98c5a187a55f27eb1ec7f5fd36896727486e
Igor Moiseyenko <imoiseyenko93@gmail.com>
2018-03-12 01:30:29 +0300

    server.js as Composition Root: the place where modules are composed together.

--- server.js
+++ server.js
@@ -3,22 +3,33 @@ require('dotenv').config()
 const Hapi = require('hapi')
 const WebSocket = require('ws')
 const config = require('config')
-const api = require('./api')
-const service = require('./service')
+const apiLayer = require('./api')
+const serviceLayer = require('./service')
+const repositoryLayer = require('./repository')
 const ws = require('./ws')
 const aop = require('./aop')
+const { Pool } = require('pg')
 
 const { port } = config.get('server')
 const { port: wssPort } = config.get('wss')
+const { DB_HOST, DB_PORT, DB_DATABASE, DB_USER, DB_PASSWORD } = process.env
+
+const dbPool = new Pool({
+  host: DB_HOST,
+  port: DB_PORT,
+  database: DB_DATABASE,
+  user: DB_USER,
+  password: DB_PASSWORD,
+})
 
 const server = new Hapi.Server()
 server.connection({ port })
-api.init(server, service)
 
-const wss = new WebSocket.Server({ port: wssPort })
-ws.init(wss)
-
-Promise.all([aop.init({ server })])
+Promise.resolve()
+  .then(() => repositoryLayer.init(dbPool))
+  .then(repository => serviceLayer.init(repository))
+  .then(service => apiLayer.init(server, service))
+  .then(() => aop.init({ server }))
   .then(() => {
     server.start(err => {
       if (err) throw err
@@ -27,4 +38,7 @@ Promise.all([aop.init({ server })])
   })
   .catch(err => {
     server.log(err)
-  })
+  });
+
+const wss = new WebSocket.Server({ port: wssPort })
+ws.init(wss)
