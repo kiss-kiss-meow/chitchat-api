8be9e4682bb1719a18e2127ee205d0a9a9e262cf
Igor Moiseyenko <imoiseyenko93@gmail.com>
2018-09-17 00:00:25 +0300

    Auth controller: Destructuring fix.

--- src/api/auth/auth.controller.js
+++ src/api/auth/auth.controller.js
@@ -7,13 +7,13 @@ class AuthController {
     return new AuthController(service)
   }
 
-  signin({ payload = {} }) {
-    const { email, password } = payload
+  signin({ payload }) {
+    const { email, password } = payload || {}
     return this.authService.signin(email, password)
   }
 
-  signup({ payload = {} }) {
-    const { email, password } = payload
+  signup({ payload }) {
+    const { email, password } = payload || {}
     return this.authService.signup(email, password)
   }
 }
